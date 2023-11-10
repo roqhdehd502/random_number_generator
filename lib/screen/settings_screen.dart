148c8fcb694374e197f6d7cf5d4cbf27bfe3f133
MinwooNa <mwna@spacecl.co.kr>
2023-11-10 16:33:00 +0900

    Initial commit

---
+++ lib/screen/settings_screen.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:random_number_generator/component/number_row.dart';
+import 'package:random_number_generator/constant/color.dart';
+
+class SettingsScreen extends StatefulWidget {
+  final int maxNumber;
+
+  const SettingsScreen({
+    required this.maxNumber,
+    super.key,
+  });
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  double maxNumber = 1000;
+
+//  부모에서 받은 State를 지정 (Flutter의 생명주기)
+  @override
+  void initState() {
+    super.initState();
+
+    maxNumber = widget.maxNumber.toDouble();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: PRIMARY_COLOR,
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            _Body(
+              maxNumber: maxNumber,
+            ),
+            _Footer(
+              maxNumber: maxNumber,
+              onSliderChanged: onSliderChanged,
+              onButtonPressed: onButtonPressed,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onSliderChanged(double val) {
+    setState(() {
+      maxNumber = val;
+    });
+  }
+
+  void onButtonPressed() {
+    Navigator.of(context).pop(maxNumber.toInt());
+  }
+}
+
+class _Body extends StatelessWidget {
+  final double maxNumber;
+
+  const _Body({required this.maxNumber, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: NumberRow(
+      number: maxNumber.toInt(),
+    ));
+  }
+}
+
+class _Footer extends StatelessWidget {
+  final double maxNumber;
+  final ValueChanged<double>? onSliderChanged;
+  final VoidCallback onButtonPressed;
+
+  const _Footer({
+    required this.maxNumber,
+    required this.onSliderChanged,
+    required this.onButtonPressed,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Slider(
+          value: maxNumber,
+          min: 1000,
+          max: 100000,
+          onChanged: onSliderChanged,
+        ),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: RED_COLOR,
+          ),
+          onPressed: onButtonPressed,
+          child: Text(
+            '저장',
+          ),
+        )
+      ],
+    );
+  }
+}
